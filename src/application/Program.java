ae5dfa7f7168feb325006cf0855e07440a281f02
Samuel Marcos <samuelmarcos9580@gmail.com>
2018-10-29 15:00:19 -0300

    Acrescentando um for muito louco

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Program {
 	
 	public static void main(String [] args) {
@@ -7,6 +10,14 @@ public class Program {
 		System.out.println("Bom dia !"); 
 		System.out.println("Boa tarde !");
 		System.out.println("Boa noite !");
+		
+		Instant startTime = Instant.now();
+		for (int i = 0; i < 10000; i ++) {
+			System.out.println("Console.log :" + Math.pow(i, 3));
+		}
+		Instant endTime = Instant.now();
+		Duration totalTime = Duration.between(startTime, endTime);
+		System.out.println("Tempo de execução :" + totalTime);
 		
 	}
 
